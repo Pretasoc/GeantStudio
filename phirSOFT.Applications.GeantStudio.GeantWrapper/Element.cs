70a8d544cf8c7f0893ffd289fc8dfecdcfe37540
Philemon Eichin <phileicht@gmx.de>
2017-06-10 17:01:29 +0200

    Add Element

---
+++ phirSOFT.Applications.GeantStudio.GeantWrapper/Element.cs
@@ -0,0 +1,67 @@
+﻿using phirSOFT.Applications.GeantStudio.GeantWrapper.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+using Nito.ConnectedProperties;
+
+namespace phirSOFT.Applications.GeantStudio.GeantWrapper
+{
+    [ContentProperty("Isotopes")]
+    [PublicAPI]
+    public class Element : SupportInitializeBase
+    {
+        private double _effectiveA;
+        private double _effectiveZ;
+        private const string RelativeAbundanceKey = "RelativeAbundance";
+
+        public Element()
+        {
+            Isotopes = new Collection<Isotope>();
+        }
+
+        public string Label { get; set; }
+
+        public string Symbol { get; set; }
+
+        public double EffectiveZ
+        {
+            get => _effectiveZ;
+            set => Set(ref _effectiveZ, value);
+        }
+
+        public double EffectiveA
+        {
+            get => _effectiveA;
+            set => Set(ref _effectiveA, value);
+        }
+
+        public ICollection<Isotope> Isotopes { get; }
+
+
+        public static double GetRelativeAbundance(Isotope target)
+        {
+            try
+            {
+                return ConnectedProperty.Get(target, RelativeAbundanceKey);
+            }
+            catch (InvalidOperationException)
+            {
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public static void SetRelativeAbundance(Isotope target, double value)
+        {
+            ConnectedProperty.Set(target, RelativeAbundanceKey, value);
+        }
+    }
+}
