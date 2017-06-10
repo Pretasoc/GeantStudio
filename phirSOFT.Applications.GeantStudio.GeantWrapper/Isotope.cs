aa7c55d230212c32a0d4bb3e5e7acb74796dab45
Philemon Eichin <phileicht@gmx.de>
2017-06-10 16:08:21 +0200

    Refactor

--- phirSOFT.Applications.GeantStudio.GeantWrapper/Isotope.cs
+++ phirSOFT.Applications.GeantStudio.GeantWrapper/Isotope.cs
@@ -1,10 +1,5 @@
 ﻿using phirSOFT.Applications.GeantStudio.GeantWrapper.Annotations;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace phirSOFT.Applications.GeantStudio.GeantWrapper
 {
